fa067ae0924c01feef57c2f8f3a1088f98618300
Luis Muskus <lmuskus@lsv-tech.com>
2022-08-11 19:13:56 -0500

    App completa con provider como gestor de estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/providers/theme_provider.dart';
+import 'package:preferences_app/screens/screens.dart';
 
-import 'screens/screens.dart';
+import 'package:preferences_app/share_preferences/preferencias.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp( const MyApp());
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Preferences.init();
+
+  runApp( 
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => ThemeProvider(isDarkmode: Preferences.isDarkmode )  )
+      ],
+      child: const MyApp(),
+    )
+  );
+}
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: HomeScreen.routerName,
       routes: {
-        HomeScreen.routerName: ( _ ) => const HomeScreen(),
-        SettingsScreen.routerName: ( _ ) => const SettingsScreen()
-        },
-     
+        HomeScreen.routerName    : ( _ ) => const HomeScreen(),
+        SettingsScreen.routerName: ( _ ) => const SettingsScreen(),
+      },
+      theme: Provider.of<ThemeProvider>(context).currentTheme,
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
-
+import 'package:preferences_app/share_preferences/preferencias.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
 
-  static const String  routerName ="Home";
+  static const String routerName = 'Home';
 
+   
   const HomeScreen({Key? key}) : super(key: key);
-
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Home Screen'),
-     ),
-   );
+      drawer: const SideMenu(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('isDarkmode: ${ Preferences.isDarkmode }'),
+          const Divider(),
+          Text('Género: ${ Preferences.gender }'),
+          const Divider(),
+          Text('Nombre de usuario: ${ Preferences.name } '),
+          const Divider(),
+        ],
+      )
+    );
   }
 }
